8795ca733e48e8611b92d60deceb621a9779cf50
YoshiHack <jwhughes2012@icloud.com>
2025-08-10 17:55:28 -0400

    Create data.asm

---
+++ src/data.asm
@@ -0,0 +1,153 @@
+; Copyright 2015-2024 Matt "MateoConLechuga" Waltz
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are met:
+;
+; 1. Redistributions of source code must retain the above copyright notice,
+;    this list of conditions and the following disclaimer.
+;
+; 2. Redistributions in binary form must reproduce the above copyright notice,
+;    this list of conditions and the following disclaimer in the documentation
+;    and/or other materials provided with the distribution.
+;
+; 3. Neither the name of the copyright holder nor the names of its contributors
+;    may be used to endorse or promote products derived from this software
+;    without specific prior written permission.
+;
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+; ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+; LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+; SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+; INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+; CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+; ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+; POSSIBILITY OF SUCH DAMAGE.
+
+; unrelocated data
+
+; safely unarchive a varible and restore screen mode
+; in the case of a garbage collect
+; returns nz if okay, z if garbage collect
+; derived from https://github.com/calc84maniac/tiboyce/blob/350e414dfc345d5e754eb87c1b87bc4e06131e71/tiboyce.asm#L468
+yoshiti.Arc_Unarc:
+	call	ti.ChkFindSym
+	ret	c
+	ex	de,hl
+	push	hl
+	add	hl,hl
+	pop	hl
+	jr	nc,.no_garbage_collect
+	ld	hl,(hl)
+	ld	a,c
+	add	a,12
+	ld	c,a
+	ld	b,0
+	add.s	hl,bc
+	jr	c,.garbage_collect
+	push	hl
+	pop	bc
+	call	ti.FindFreeArcSpot
+	jr	nz,.no_garbage_collect
+.garbage_collect:
+	xor	a,a
+	push	af
+	call	ti.boot.ClearVRAM
+	ld	a,$2d
+	ld	(ti.mpLcdCtrl),a
+	call	ti.DrawStatusBar
+	jr	.archive_or_unarchive
+.no_garbage_collect:
+	xor	a,a
+	inc	a
+	push	af
+.archive_or_unarchive:
+	ld	hl,data_lcd_init
+	call	ti.PushErrorHandler
+	call	ti.Arc_Unarc
+	call	ti.PopErrorHandler
+data_lcd_init:
+	call	ti.RunIndicOff
+	di				; turn off indicator
+	call	ti.boot.ClearVRAM
+	ld	de,ti.mpLcdPalette	; address of mmio palette
+	ld	b,e			; b = 0
+.loop:
+	ld	a,b
+	rrca
+	xor	a,b
+	and	a,224
+	xor	a,b
+	ld	(de),a
+	inc	de
+	ld	a,b
+	rla
+	rla
+	rla
+	ld	a,b
+	rra
+	ld	(de),a
+	inc	de
+	inc	b
+	jr	nz,.loop		; loop for 256 times to fill palette
+	pop	af
+	ld	hl,ti.vRam
+	ld	bc,((ti.lcdWidth * ti.lcdHeight) * 2) + 0
+	ld	a,0xff
+	call	ti.MemSet
+	ld	a,ti.lcdBpp8
+	ld	(ti.mpLcdCtrl),a	; operate in 8bpp
+	ret
+
+yoshiti_cleanup:
+	ld	a,$25
+	ld	($D02687),a
+	xor	a,a
+	ld	(ti.menuCurrent),a
+	ld	(ti.appErr1),a
+	ld	(ti.kbdGetKy),a
+	ld	hl,ti.textShadow
+	ld	de,ti.cmdShadow
+	ld	bc,$104
+	ldir
+	ld	hl,ti.pixelShadow
+	ld	bc,8400 * 3
+	call	ti.MemClear
+    call	ti.ForceFullScreen
+	jp	ti.ClrWindow
+
+data_cesium_appvar:
+	db	ti.AppVarObj
+data_string_cesium_name:
+	db	yoshiti_name,0
+
+
+if language eq "english"
+data_string_password:
+	db	'Password:',0
+end if
+
+if language eq "french"
+data_string_password:
+	db	'Mot de passe:',0
+end if
+
+if language eq "dutch"
+data_string_password:
+    db  'Wachtwoord:',0
+end if
+
+if language eq "italian"
+data_string_password:
+	db	'Parola d',$27,'ordine:',0
+end if
+
+data_string_quit1:
+	db	'1:',0,'Quit',0
+data_string_quit2:
+	db	'2:',0,'Goto',0
+
+; data in this location is allowed to be modified at runtime
+	app_data
